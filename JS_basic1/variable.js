a8b9e9f8b5a1a3ebcd539200dd822d7c2a638e69
Morning <morning@admins-iMac-3.local>
2022-11-28 12:37:43 +0800

    JS

---
+++ JS_basic1/variable.js
@@ -0,0 +1,90 @@
+// 1    
+    //"Comments can make code readable"
+// 2
+    //"Javascript lesson begin"
+// 3
+    /*"Comments can make 
+    ode readable"*/
+// 4
+    let a = 23;
+    console.log("hello world",a);
+    let b = "23";
+    console.log(b);
+    let c ;
+    console.log(a);
+    let d = true;
+    let e = null; 
+    
+// 5
+    console.log(typeof (a))
+    console.log(typeof (b))
+    console.log(typeof (c))
+    console.log(typeof (d))
+    console.log(typeof (e))
+    
+// 6
+    let coco ;
+
+// 7 
+    let kiki = "Hi";
+
+// 8 
+    let firstName;
+    let lastName; 
+    let maritalStatus;
+    let country;
+    let age;
+
+// 9 
+    let FirstName, LastName, MaritalStatus, Country , Age;
+    console.log(firstName, lastName, maritalStatus, country, age);
+
+// 10
+    let MyAge = 23;
+    let YourAge = 27;
+
+// 11
+    let TriangleA = 3;
+    let TriangleB = 4;
+    let TriangleC = 5;
+    let TriangleS = TriangleA + TriangleB + TriangleC;
+    console.log(TriangleS)
+
+// 12 
+    let TriangleP = 3.14;
+    let TriangleR = 8;
+    let TriangleCirkleS = TriangleP + TriangleR ** 2;
+    console.log(TriangleCirkleS)
+
+// 13  m = y2-y1/x2-x1
+    let y2 = 10;
+    let y1 = 4;
+    let x2 = 9;
+    let x1 = 3;
+    let m = (y2 - y1) / (x2 - x1);
+    console.log(m)
+
+// 14
+    let gegabyte = 15;
+    let megabyte = 1024;
+    let kilobyte = 1024;
+    let byte = 1024;
+    let bit = 8;
+    let convertbit = gegabyte * megabyte * kilobyte * byte * bit;
+    console.log(convertbit)
+
+//  15
+    let Megabyte = 15;
+    let Kilobyte = 1024;
+    let Byte = 1024;
+    let Bit = 8;
+    let Rate = 3;
+    let BitRate = Megabyte * Kilobyte * Byte * Bit / Rate;
+    console.log(BitRate)
+
+// 16
+    let side1 = 15;
+    let side2 = 17;
+    let side3 = 13;
+    let TrngleS = side1 + side2 + side3;
+    console.log(TrngleS);
