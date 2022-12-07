8f9afd0edf3409e36c90f836b37bfa3799050fd1
khuderbaatar Tamir <khuderbaatartamir@khuderbaatars-MacBook-Pro.local>
2022-12-07 10:17:01 +0800

    home

---
+++ JS_Array/Array_of_numbers.js
@@ -0,0 +1,33 @@
+// 1
+
+let arrayNumbers = [43, 56, 23, 89, 88, 90, 99, 652, 15, 290, 11];
+let i = 0;
+let sum = 0;
+while (i < arrayNumbers.length) {
+    sum = sum + arrayNumbers[i];
+    i++
+} console.log(sum);
+
+// 2
+let c = 0;
+let max = 0;
+while (c < arrayNumbers.length) {
+    if (max < arrayNumbers[c]) {
+        max = arrayNumbers[c]
+    }
+    c++
+}
+console.log(max);
+
+// 3 
+
+c = 0;
+let min = 43;
+while (c < arrayNumbers.length) {
+    if (min > arrayNumbers[c]) {
+        min = arrayNumbers[c]
+    }
+    c++
+}
+console.log(min);
+
